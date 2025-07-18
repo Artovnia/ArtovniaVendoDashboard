b8acc0f2add76784bd573f6ee2f48537a9b31866
Artovnia <sayuri.platform@gmail.com>
2025-07-18 18:59:23 +0200

    Add new feature X

--- src/routes/payout/index.tsx
+++ src/routes/payout/index.tsx
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 const PayoutDetail = lazy(() => import('./payout-detail'));
 const PayoutCreate = lazy(() => import('./payout-create'));
 const PayoutEdit = lazy(() => import('./payout-edit'));
-const SimplifiedPayoutForm = lazy(() => import('./simplified-payout-form')); // Keep for backwards compatibility
+
 
 /**
  * Main payout routing component
@@ -17,7 +17,7 @@ const PayoutRoutes = () => {
       <Route path="detail" element={<PayoutDetail />} />
       <Route path="create" element={<PayoutCreate />} />
       <Route path="edit/:section" element={<PayoutEdit />} />
-      <Route path="simplified" element={<SimplifiedPayoutForm />} /> {/* Legacy route */}
+      
     </Routes>
   );
 };
